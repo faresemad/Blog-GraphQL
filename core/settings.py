4a066da4f4513a3764ef9acbf56be3e101b1a91b
Fares Emad <faresemadx@gmail.com>
2024-07-23 17:01:47 +0300

    Update INSTALLED_APPS in settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,10 +15,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -26,6 +23,12 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = ["graphene_django"]
+
+LOCAL_APPS = ["blog"]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
