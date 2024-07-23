1fe66afc106cd867f4799770d4d1e865b966f6d6
Fares Emad <faresemadx@gmail.com>
2024-07-23 17:04:00 +0300

    Craete models.py to include Author and Post models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
-from django.db import models  # noqa
+# models.py
+from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
